fe604393b7835b5b31ffc52a4eaac98c5a3dc937
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-02-02 10:45:36 -0300

    Aula-4 02/02/2024

---
+++ JavaScript/HashtagChat/servidor.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+const servidorHttp = http.createServer(app);
+const io = require('socket.io')(servidorHttp);
+
+io.addListener('connection', (socket) => {
+    console.log('\nUsuário Conectado!\n');
+    socket.addListener('nova mensagem', (msg) => {
+        io.emit('nova mensagem', msg);
+
+    });
+
+});
+
+app.use(express.static('public'));
+
+servidorHttp.listen(1000, '10.3.152.20', () => {
+    console.log('\nServidor está escutando na porta 1000!\n');
+
+});
